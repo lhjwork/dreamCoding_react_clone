89280dbaad5fb43a7d83d5ce4cadcc1b820d1cdf
lhjwork <jin.come.up.business@gmail.com>
2023-07-11 13:41:07 +0900

    State를 객체  단위로 사용하고 적용하는 방법

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -5,14 +5,24 @@ export default function AppXY() {
   // const [x,setX] = useState(0);
   // const [y,setY] = useState(0);
   // 서로 밀접한 연관성을 가진 데이터라면 이렇게 객체로 다루는 것이 효율 적임
-  const [position, setPostion] = useState({x:0, y:0});
+  const [position, setPostion] = useState({x:0, y:500 , z:0});
 
 
   return (
     <div className='container' onPointerMove={(e)=>{
-      setPostion({x:e.clientX, y:e.clientY})
+
         // setX(e.clientX);
         // setY(e.clientY);
+
+        // setPostion({x:e.clientX, y:e.clientY})
+
+        // 만약 수평으로만 이동이 가능하다면
+        // prev.y 이전 상태값만 가져 오도록 함
+        // setPostion(prev => ({x:e.clientX, y:prev.y}));
+
+        // 객체의 변수가 여러개일때 
+        setPostion(prev => ({...prev, x:e.clientX,}));
+
     }}>
       {/* <div className='pointer' style={{transform:`translate(${x}px, ${y}px)`}}/> */}
       <div className='pointer' style={{transform:`translate(${position.x}px, ${position.y}px)`}}/>
