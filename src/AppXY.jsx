1873a46435bc2afed5321d727d702e28b14d8275
lhjwork <jin.come.up.business@gmail.com>
2023-07-11 13:36:12 +0900

    수평으로 만 이동이 가능

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -2,15 +2,20 @@ import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
-  const [x,setX] = useState(0);
-  const [y,setY] = useState(0);
+  // const [x,setX] = useState(0);
+  // const [y,setY] = useState(0);
+  // 서로 밀접한 연관성을 가진 데이터라면 이렇게 객체로 다루는 것이 효율 적임
+  const [position, setPostion] = useState({x:0, y:0});
+
 
   return (
     <div className='container' onPointerMove={(e)=>{
-        setX(e.clientX);
-        setY(e.clientY);
+      setPostion({x:e.clientX, y:e.clientY})
+        // setX(e.clientX);
+        // setY(e.clientY);
     }}>
-      <div className='pointer' style={{transform:`translate(${x}px, ${y}px)`}}/>
+      {/* <div className='pointer' style={{transform:`translate(${x}px, ${y}px)`}}/> */}
+      <div className='pointer' style={{transform:`translate(${position.x}px, ${position.y}px)`}}/>
     </div>
   );
 }
